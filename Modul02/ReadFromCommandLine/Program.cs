14883b8cbf90ec3cdb11b6925d4813ca8c409c88
Лъчезар Петков <lu4i@abv.bg>
2015-08-20 00:23:32 +0300

    Modul 02, Video 06 - Rabota sas stringove

--- Modul02/ReadFromCommandLine/Program.cs
+++ Modul02/ReadFromCommandLine/Program.cs
@@ -16,6 +16,19 @@ namespace ReadFromCommandLine
 
 			//Резулатат
 			Console.WriteLine ("Резулата а+б е "+ (a+b).ToString ()+"\n\n\n");
+
+			string test = "1";
+
+			test += ", 2";
+			test += ", 333";
+			test += ", 4";
+			test += ", 5";
+			test += ", 6";
+
+			Console.WriteLine ("Събиране с += " + test+ "\n");
+			test = test.Replace (", " , "-");
+			Console.WriteLine ("TEST - Replace : " + test + "\n");
+			Console.WriteLine ("3ти знак e " + test.Split('-')[2]+ "\n");
 		}
 	}
 }
