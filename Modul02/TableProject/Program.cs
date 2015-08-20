3d69176326b97aaa9d5947ffd5d1a54ea4a68c42
Лъчезар Петков <lu4i@abv.bg>
2015-08-20 23:37:54 +0300

    Modul 02, Video 09 - Rabota s ENUM

--- Modul02/TableProject/Program.cs
+++ Modul02/TableProject/Program.cs
@@ -4,28 +4,29 @@ namespace TableProject
 {
 	class MainClass
 	{
+		enum ti {ime=0, familia, godini};
 		public static void Main (string[] args)
 		{
 			//Дефиниране таблица
 			string[,] table = new string[2,3];
 
 			//въвеждане на 1 ви ред
-			table[0,0] = "Лъчезар";
-			table[0,1] = "Пламенов";
-			table[0,2] = "37";
+			table[0, (int)ti.ime] = "Лъчезар";
+			table[0, (int)ti.familia] = "Пламенов";
+			table[0, (int)ti.godini] = "37";
 
 
 			//Въвеждане на 2 ри ред
-			table[1,0] = "Пламен";
-			table[1,1] = "Лъчезаров";
-			table[1,2] = "35";
+			table[1, (int)ti.ime] = "Пламен";
+			table[1, (int)ti.familia] = "Лъчезаров";
+			table[1, (int)ti.godini] = "35";
 
 
 			//Печат
 			Console.Write("Кой ред искате да видите: " );
 			int _index = Convert.ToInt32 (Console.ReadLine ())-1;
 
-			Console.WriteLine ((table[_index,0])[0] +". " + table[_index,1] + ", " + table[_index,2] + "г.");
+			Console.WriteLine ((table[_index,(int)ti.ime])[0] +". " + table[_index,(int)ti.familia] + ", " + table[_index,(int)ti.godini] + "г.");
 		}
 	}
 }
