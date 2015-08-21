2e9da648dab112327bee133d6d48e9b04f06d496
Лъчезар Петков <lu4i@abv.bg>
2015-08-21 12:58:56 +0300

    Модул 03, Видео 04 - Променливи от тип BOOL

--- Modul03/BoolExplore/Program.cs
+++ Modul03/BoolExplore/Program.cs
@@ -6,7 +6,16 @@ namespace BoolExplore
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			int[] _i = new int[] { 4, 3, 2 };
+
+			int _ui = Convert.ToInt32 (Console.ReadLine());
+
+			bool _chek01 = _ui <= _i.Length;
+			bool _chek02 = _ui > 0;
+
+			Console.WriteLine (_chek01);
+			Console.WriteLine (_chek02);
+			Console.WriteLine (_i[_ui - 1] );
 		}
 	}
 }
