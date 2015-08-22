ad14cfea8c0568a082b279dd979e8a4c5f3b5da9
Лъчезар Петков <lu4i@abv.bg>
2015-08-22 10:44:53 +0300

    Модул 03, Видео 05 - Булеви операции

--- Modul03/BoolExplore/Program.cs
+++ Modul03/BoolExplore/Program.cs
@@ -13,9 +13,16 @@ namespace BoolExplore
 			bool _chek01 = _ui <= _i.Length;
 			bool _chek02 = _ui > 0;
 
-			Console.WriteLine (_chek01);
-			Console.WriteLine (_chek02);
-			Console.WriteLine (_i[_ui - 1] );
+			bool _exp01 = _chek01 && _chek02;   // Логическо И - "&&" 
+			bool _exp02 = _chek01 || _chek02;   // Логическо ИЛИ - "||"
+			bool _exp03 = !_chek01;  //Отрицание
+
+			Console.WriteLine ("ЛОгическо И: " + _exp01.ToString());
+			Console.WriteLine ("ЛОгическо ИЛИ: " + _exp02.ToString());
+			Console.WriteLine ("Отрицание на " + _chek01.ToString() + " : " + _exp03);
+
+
+			//Console.WriteLine (_i[_ui - 1] );
 		}
 	}
 }
