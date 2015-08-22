c74aac7664c6e4c6a92ff4debf661ff541399c74
Лъчезар Петков <lu4i@abv.bg>
2015-08-22 20:57:17 +0300

    Модул 05, Видео 01 - Въведение в методите

--- Modul05/Modul05/Program.cs
+++ Modul05/Modul05/Program.cs
@@ -17,6 +17,33 @@ namespace Modul05
 				Console.Write("$ "); _usercommand = Console.ReadLine();
 				if (_usercommand.Contains("cmd01")) cmd01 (_usercommand);
 				if (_usercommand.Contains("cmd02")) Console.WriteLine(cmd02 (_usercommand));
+				if (_usercommand.Contains("cmd03")) 
+					{
+						double a=0;
+						if (cmd03(_usercommand, out a))
+						{
+							Console.WriteLine("a . a = " + a.ToString());
+						}
+						else
+						{
+							Console.WriteLine("Командата не е въведена коректно");	
+						}
+
+					}
+				if (_usercommand.Contains("cmd04")) 
+				{
+					double _t = 5, _c = -2;
+					if (cmd04 (ref _t))
+					{
+						Console.WriteLine(_t);
+					}
+					if (cmd04 (ref _c))
+					{
+						Console.WriteLine(_t);
+					}
+				}
+
+
 			}
 			while(_usercommand != "exit");
 		}
@@ -28,6 +55,10 @@ namespace Modul05
 				if (double.TryParse(_input.Split(' ')[1],out _temp))
 				{
 					Console.WriteLine("\nРезултатът от " + _temp.ToString() + "^2 e:" + (_temp *_temp).ToString());
+				}
+				else
+				{
+					Console.WriteLine("Командата не е въведена коректно");	
 				}
 			}catch{}
 		}
@@ -41,7 +72,37 @@ namespace Modul05
 				_temp = _temp * _temp * _temp;
 
 			}
+			else
+			{
+				Console.WriteLine ("Командата не е въведена коректно");	
+			}
 			return _temp;
+		}
+		public static bool cmd03 (string _input, out double _i)   // cmd03 45  --> 45*45
+		{
+			try
+			{
+				string _p = _input.Split(' ')[1];
+				double __p = 0;
+				if (double.TryParse(_p, out __p) )
+				{
+					_i = __p  *  __p;
+					return true;
+				}
+
+			}catch{}
+
+		_i = 0;
+		return false;
+		}
+		public static bool cmd04 (ref double _i)
+		{
+			double _temp = _i;
+			if (_i > 0) {
+				_i = _i * _i;
+
+			} 
+			return (_temp > 0);
 		}
 	}
 }
