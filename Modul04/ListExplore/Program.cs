baaf287d60b3017b505d662d93d943715e35ff40
Лъчезар Петков <lu4i@abv.bg>
2015-08-22 14:41:17 +0300

    Модул 04, Видео 6 - Примери на цикъл do... while

---
+++ Modul04/ListExplore/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ListExplore
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			// Деефиниции
+			string _userinput = "";
+
+			//потребителски вход
+			do 
+			{
+				Console.Write("? ");
+				_userinput = Console.ReadLine();
+			}
+			while(_userinput != "exit");
+		}
+	}
+}
