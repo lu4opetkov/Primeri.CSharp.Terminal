86b9b3d65564f7efa6cd818e61d078f016aa2c8f
Лъчезар Петков <lu4i@abv.bg>
2015-08-22 19:23:20 +0300

    Модул 05, Видео 01 - Въведение в методите

---
+++ Modul05/Modul05/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Modul05
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			//Дефиниции
+
+			string _usercommand = "";
+
+			//Потребителски команди
+
+			do
+			{
+				Console.Write("$ "); _usercommand = Console.ReadLine();
+				if (_usercommand.Contains("cmd01")) cmd01 (_usercommand);
+				if (_usercommand.Contains("cmd02")) Console.WriteLine(cmd02 (_usercommand));
+			}
+			while(_usercommand != "exit");
+		}
+		public static void cmd01 (string _input) // cmd01 5 - 5^2
+		{
+			try 
+			{
+				double _temp = 0;
+				if (double.TryParse(_input.Split(' ')[1],out _temp))
+				{
+					Console.WriteLine("\nРезултатът от " + _temp.ToString() + "^2 e:" + (_temp *_temp).ToString());
+				}
+			}catch{}
+		}
+
+
+		public static int cmd02 (string _input) // cmd02 5 - 5^3
+		{
+			int _temp = 0;
+			if (int.TryParse(_input.Split(' ')[1],out _temp))
+			{
+				_temp = _temp * _temp * _temp;
+
+			}
+			return _temp;
+		}
+	}
+}
